c017be8eb12d41c59b4c08f80586a27b6b2941b8
Eva <78699683+evieem28@users.noreply.github.com>
2022-01-29 13:59:40 +0000

    changes to css file and weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -2,40 +2,64 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
+  let weatherData = {
+    city: "London",
+    temperature: "15",
+    date: "Saturday 13:00",
+    description: "Partly Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+    humidity: "75",
+    wind: "8",
+  };
+
   return (
     <div className="Weather">
-      <form>
+      <form className="mb-3">
         <div className="row">
           <div className="col-9">
             <input
               type="search"
-              placeholder="Enter a city.."
+              placeholder="Type a city.."
               className="form-control"
+              autoComplete="off"
             />
           </div>
           <div className="col-3">
-            <input type="sumbit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Saturday 07:00</li>
-        <li>Snowstorm</li>
-      </ul>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
       <div className="row">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/snow_heavy.png"
-            alt="Snowing"
-          />
-          -8°C
+          <div className="clearfix weather-temperature">
+            <img
+              src={weatherData.imgUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+            <div className="float-left">
+              <strong>{weatherData.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>Precipitation: 92%</li>
-            <li>Humidity: 80%</li>
-            <li>Wind: 21mph</li>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind} km/h</li>
           </ul>
         </div>
       </div>
