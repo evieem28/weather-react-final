b9583b4e374a7673fb28a6d71dad78712c369217
Eva <78699683+evieem28@users.noreply.github.com>
2022-01-29 12:47:26 +0000

    added github rep link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded By Eva Sznajder and is{" "}
+        <a
+          href="https://github.com/evieem28/weather-react-final"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
